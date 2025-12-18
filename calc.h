4d948ac7e920cfe7fb5dc64bc0a7ed2b7707e1a5
ISDNightNova <nightnovann@gmail.com>
2025-12-18 23:47:22 +0530

    Create calc.h

---
+++ calc.h
@@ -0,0 +1,16 @@
+#ifndef CALC_H
+#define CALC_H
+
+// Function prototypes for basic arithmetic operations
+
+int add(int a, int b);
+int sub(int a, int b);
+int mul(int a, int b);
+int divide(int a, int b);
+int fac(int n);
+int mod(int a, int b);
+int power(int base, int exp);
+int gcd(int a, int b);
+int lcm(int a, int b);
+
+#endif // CALC_H
